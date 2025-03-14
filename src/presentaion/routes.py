4ec4dcda3724cba2ef0963925ccbdfcd17e6228b
Mohammad Souri <mohammad.souri@sociumtech.com>
2025-03-14 15:37:56 +0300

    done

--- src/presentaion/routes.py
+++ src/presentaion/routes.py
@@ -6,6 +6,6 @@ from src.presentaion.student import StudentView
 def register_routes(app: Flask) -> None:
 
     student_view = StudentView.as_view('student_view')
-    
+
     app.add_url_rule('/students', view_func=student_view, methods=['GET', 'POST'])
     app.add_url_rule('/students/<int:id>', view_func=student_view, methods=['GET', 'PUT', 'DELETE'])
